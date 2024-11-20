f997ebe7b09f27c96048a4e98b0cf571ceec9d7e
pieles.florian@gmail.com <pieles.florian@gmail.com>
2024-11-20 10:57:37 +0000

    Edited backend

--- server_code/backend.py
+++ server_code/backend.py
@@ -7,5 +7,39 @@ import anvil.server
 import sqlite3
 
 @anvil.server.callable
+def verbinde_db():
+    conn = sqlite3.connect('gefaengnis.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@anvil.server.callable
 def get_gefaengnisse():
-  return [('TODO 1', 1), ('TODO 2', 2)]
+  conn = verbinde_db()
+  cursor = conn.cursor()
+
+  cursor.execute("SELECT * FROM Gefaengnis")
+  res = [dict(row) for row in cursor.fetchall()]
+  conn.close()
+  return res
+
+@anvil.server.callable
+def get_zelle(gefaengnis_id):
+  conn = verbinde_db()
+  cursor = conn.cursor()
+
+  cursor.execute("""SELECT Zellennummer, Belegt, Aktuelle_Belegung FROM Zelle WHERE GID = ?""",(gefaengnis_id))
+  res = [dict(row) for row in cursor.fetchall()]
+  conn.close()
+  return res
+
+def get_haeftlinge(zelle_id):
+    conn = verbinde_db()
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT H.Name, H.Haftdauer, B.Einzug, B.Auszug
+        FROM Haeftling H
+        JOIN Bewohntzeit B ON H.HID = B.HID
+        WHERE B.ZID = ?""", (zelle_id,))
+    res = [dict(row) for row in cursor.fetchall()]
+    conn.close()
+    return res
